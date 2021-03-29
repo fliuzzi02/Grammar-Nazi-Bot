5c3b706c8a64988b9a4c94954d53fbe7cdaba4a8
fliuzzi02 <fliuzzi02.gmail.com>
2021-03-29 19:03:51 +0200

    First commit

---
+++ correction.py
@@ -0,0 +1,21 @@
+from spellchecker import SpellChecker
+from autocorrect import Speller
+
+t = Speller('it')
+spell = SpellChecker()
+
+def fun(phrase):
+    print(phrase)
+    words = spell.split_words(phrase)
+    misspelled = []
+
+    for word in words:
+        word.lower()
+        corrected = t(word)
+        if word == corrected:
+            #print("word is correct")
+        else:
+            print(word+" = "+corrected)
+            misspelled.append(word)
+
+    return misspelled
---
+++ main.py
@@ -0,0 +1,46 @@
+import discord
+import os
+from correction import fun
+
+#Bot Token
+token = 'ODI1MTAzMDg4NTc4ODU0OTUy.YF5DIQ.9yXudu4Nzy8X8HVnVtGRUMqOk9g'
+
+#About
+about = discord.Embed()
+about.title = 'Grammar Nazi'
+about.description = 'Bot by @desmofede42'
+
+#Help
+helpCom = discord.Embed()
+helpCom.title = "Available commands"
+helpCom.description = "Bot's invocation symbol is ``g!``"
+
+
+#Client Variables
+client = discord.Client()
+client.running = False
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('g!start') and not client.running:
+        client.running = True
+        await message.channel.send("Ora sono attivo, mio signore...")
+
+    elif message.content.startswith('g!stop') and client.running:
+        client.running = False
+        await message.channel.send("Come più gradisce, mio signore...")
+        
+    elif message.content.startswith('') and client.running:
+        misspelled = fun(message.content)
+        if len(misspelled) != 0:
+            await message.channel.send(message.author.mention+', hai sbagliato '+str(len(misspelled))+' parole, sei peggio del mio creatore...')
+
+
+client.run(token)
