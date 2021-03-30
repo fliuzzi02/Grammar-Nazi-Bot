98eecb4ead95a8fc534ba70acbdd4b6fc4efe87c
fliuzzi02 <fliuzzi02.gmail.com>
2021-03-30 09:28:13 +0200

    Translation to english

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ token = 'token here'
 about1 = discord.Embed()
 about1.title = 'Grammar Nazi'
 about1.description = 'Bot by @desmofede42'
-about1.add_field(name='Repository di GitHub', value='[Click Here](https://www.github.com/fliuzzi02/Grammar-Nazi-Bot)', inline=False)
+about1.add_field(name='GitHub Repository', value='[Click Here](https://www.github.com/fliuzzi02/Grammar-Nazi-Bot)', inline=False)
 
 #Help
 helpEmbed = discord.Embed()
 helpEmbed.title = "Available commands"
 helpEmbed.description = "Bot's invocation symbol is ``g!``"
-helpEmbed.add_field(name='Help me', value='``g!help``', inline=False)
+helpEmbed.add_field(name='Commands list', value='``g!commands``', inline=False)
 helpEmbed.add_field(name='Start monitoring', value='``g!start`', inline=False)
 helpEmbed.add_field(name='Stop monitoring', value='``g!stop`', inline=False)
 helpEmbed.add_field(name='Available languages', value='``g!lang en/it``', inline=False)
@@ -24,32 +24,34 @@ helpEmbed.add_field(name='Available languages', value='``g!lang en/it``', inline
 #Bot Commands
 bot = commands.Bot(command_prefix='g!')
 bot.running = False
-bot.language = 'it'
+bot.language = 'en'
 
 @bot.command()
 async def about(ctx):
     await ctx.channel.send(embed=about1)
 
-@bot.command(name='helpme')
-async def funky(ctx):
+@bot.command(name = "commands")
+async def funky_name(ctx):
     await ctx.channel.send(embed=helpEmbed)
 
-@bot.command(name='start')
+@bot.command()
 async def start(ctx):
     bot.running = True
     await ctx.channel.send("The bot will start, sir...")
 
-@bot.command(name='stop')
+@bot.command()
 async def stop(ctx):
     bot.running = False
     await ctx.channel.send("The bot will stop, sir...")
 
-@bot.command(name='lang')
+@bot.command()
 async def lang(ctx, arg):
     if arg == 'it':
         bot.language = 'it'
+        await ctx.channel.send("Language set to Italian")
     elif arg == 'en':
         bot.language = 'en'
+        await ctx.channel.send("Language set to English")
     else:
         await ctx.channel.send("Selected language not supported")
 
@@ -69,7 +71,7 @@ async def on_message(message):
     elif bot.running:
         misspelled = fun(message.content, bot.language)
         if len(misspelled) != 0:
-            await message.channel.send(message.author.mention+', hai sbagliato '+str(len(misspelled))+' parole, sei peggio del mio creatore...')
+            await message.channel.send(message.author.mention+', you misspelled '+str(len(misspelled))+' words, you suck!')
 
 #client.run(token)
 bot.run(token)
